45d2543d87b4fd468c249e8fc61388aa572a957f
santunis.oliveira <santunis.oliveira@sicoob.com.br>
2023-12-13 08:55:06 -0300

    ajustes no projeto

---
+++ src/main/java/br/com/facilquinze/facilquinze/FacilQuinzeApplication.java
@@ -0,0 +1,13 @@
+package br.com.facilquinze.facilquinze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacilQuinzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacilQuinzeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/facilquinze/facilquinze/FacilQuinzeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.facilquinze.facilquinze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacilQuinzeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
